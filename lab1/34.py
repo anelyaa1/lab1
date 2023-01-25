190af3fca89159ed8358e50c252320146ef69a19
anelyaa1 <123242086+anelyaa1@users.noreply.github.com>
2023-01-25 23:51:29 +0600

    Add files via upload

---
+++ lab1/34.py
@@ -0,0 +1,14 @@
+x = True
+print(type(x))
+
+y = b"Hello"
+print(type(y))
+
+z = bytearray(5)
+print(type(z))
+
+c = memoryview(bytes(5))
+print(type(c))
+
+w = None
+print(type(w))
---
+++ lab1/58.py
@@ -0,0 +1,5 @@
+b = "Hello, World!"
+print(b.upper())
+
+c = "Hello, World!"
+print(c.lower())
